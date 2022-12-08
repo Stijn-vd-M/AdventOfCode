39cfb57095cccb671287831300db44fa22aa921f
Stijn van der Meer <stijn.meer@gmail.com>
2022-12-08 13:43:05 +0100

    Day 8

---
+++ AdventOfCode/2022/Day8/TreePoint.cs
@@ -0,0 +1,20 @@
+﻿namespace Day8
+{
+	internal struct TreePoint : IEquatable<TreePoint>
+	{
+		public int X { get; }
+		public int Y { get; }
+
+
+		public TreePoint(int x, int y)
+		{
+			X = x;
+			Y = y;
+		}
+
+		public bool Equals(TreePoint other)
+		{
+			return X == other.X && Y == other.Y;
+		}
+	}
+}
